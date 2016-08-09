1b6f30e571480eda70f4d02adff029b9f6860db6
rmoncrief <rmoncri1@vols.utk.edu>
2016-08-09 09:20:48 -0400

    finished the bar graph section.

--- Project/components/Dashboard.js
+++ Project/components/Dashboard.js
@@ -7,16 +7,44 @@
 
   //  $ctrl.color = "#A62E5C";
   //  $ctrl.width = 100;
-    $ctrl.height = 30;
-    $ctrl.specs =
-      [
-        {color: '#2A9FBC', width: 50},
-        {color: '#F15B2A', width: 60},
-        {color: '#A62E5C', width: 70}
-      ];
-      
-    $scope.$watch('specs', function() {
-      $window.alert("stuff was changed")
+  //  $ctrl.height = 50;
+    $ctrl.specs ={height:30,padding:5,fontStyle:'10pt arial',
+      fontHeight:10,
+      gradientInterval: 50,
+      gradients: [],
+      bars: [
+        {color: '#2A9FBC', width: 50, text:"September"},
+        {color: '#F15B2A', width: 60, text:"October"},
+        {color: '#A62E5C', width: 90, text:"November"}
+      ]};
+
+    $scope.$watch('$ctrl.specs', function() {
+
+      var ctx = document.createElement('canvas').getContext('2d');
+      $ctrl.specs.maxWidth = 0;
+      $ctrl.specs.labelWidth = 0;
+      var gradients = [];
+
+      ctx.font = $ctrl.specs.fontStyle;
+
+      angular.forEach($ctrl.specs.bars, function(bar,index) {
+        $ctrl.specs.labelWidth = Math.max($ctrl.specs.labelWidth,
+                                          ctx.measureText(bar.text).width);
+        $ctrl.specs.maxWidth = Math.max($ctrl.specs.maxWidth,bar.width);
+      });
+
+      for (var i = 0;; i+= $ctrl.specs.gradientInterval) {
+        gradients.push({text: i, offset: i});
+        if( i > $ctrl.specs.maxWidth) {
+          break;
+        }
+      }
+
+      $ctrl.specs.labelWidth += 5;
+      $ctrl.specs.gradients = gradients;
+      $ctrl.specs.overallHeight = $ctrl.specs.bars.length *
+        (1 * $ctrl.specs.height + $ctrl.specs.padding);
+
     }, true)
 
 
